2b1dfc79da64f99118a9286efb60b464bd989587
Vlad <drygbah@gmail.com>
2015-03-26 17:01:54 +0200

    Fill Node class

--- node.cpp
+++ node.cpp
@@ -1,6 +1,10 @@
 #include "node.h"
 
-Node::Node()
+#include "graph.h"
+#include "edge.h"
+
+Node::Node(Graph *graph, int id, int weight, const QPointF &pos)
+    : _graph(graph), _id(id), _weight(weight), _pos(pos)
 {
 
 }
@@ -10,3 +14,27 @@ Node::~Node()
 
 }
 
+void Node::setWeight(int weight)
+{
+    _weight = weight;
+}
+
+void Node::setPos(const QPointF &pos)
+{
+    _pos = pos;
+}
+
+int Node::id() const
+{
+    return _id;
+}
+
+int Node::weight() const
+{
+    return _weight;
+}
+
+QPointF Node::pos() const
+{
+    return _pos;
+}
--- node.h
+++ node.h
@@ -1,12 +1,29 @@
 #ifndef NODE_H
 #define NODE_H
 
+class Graph;
+class Edge;
+
+class QPointF;
 
 class Node
 {
 public:
-    Node();
+    Node(Graph *graph, int id, int weight, const QPointF &pos = QPointF());
     ~Node();
+
+    void setWeight(int weight);
+    void setPos(const QPointF &pos);
+
+    int     id()        const;
+    int     weight()    const;
+    QPointF pos()       const;
+
+private:
+    Graph   *_graph;
+    int      _id;
+    int      _weight;
+    QPointF  _pos;
 };
 
 #endif // NODE_H
