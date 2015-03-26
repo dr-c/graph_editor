787e704f9c5b8936f93fc3fe8aa02a0d2ea086db
Vlad <drygbah@gmail.com>
2015-03-26 16:33:47 +0200

    Fill Edge and inherited classes

--- edge.cpp
+++ edge.cpp
@@ -1,6 +1,10 @@
 #include "edge.h"
 
-Edge::Edge()
+#include "graph.h"
+#include "node.h"
+
+Edge::Edge(Graph *graph, int weight, Node *from, Node *to)
+    : _graph(graph), _weight(weight), _from(from), _to(to)
 {
 
 }
@@ -10,7 +14,33 @@ Edge::~Edge()
 
 }
 
-DirectedEdge::DirectedEdge()
+void Edge::setWeight(int weight)
+{
+    _weight = weight;
+}
+
+Graph *Edge::graph() const
+{
+    return _graph;
+}
+
+int Edge::weight() const
+{
+    return _weight;
+}
+
+Node *Edge::nodeFrom() const
+{
+    return _from;
+}
+
+Node *Edge::nodeTo() const
+{
+    return _to;
+}
+
+DirectedEdge::DirectedEdge(Graph *graph, int weight, Node *from, Node *to)
+    : Edge(graph, weight, from, to)
 {
 
 }
@@ -20,7 +50,8 @@ DirectedEdge::~DirectedEdge()
 
 }
 
-UndirectedEdge::UndirectedEdge()
+UndirectedEdge::UndirectedEdge(Graph *graph, int weight, Node *from, Node *to)
+    : Edge(graph, weight, from, to)
 {
 
 }
--- edge.h
+++ edge.h
@@ -1,25 +1,41 @@
 #ifndef EDGE_H
 #define EDGE_H
 
+class Graph;
+class Node;
 
 class Edge
 {
-protected:
-    Edge();
+public:
     virtual ~Edge();
+
+    void setWeight(int weight);
+
+    Graph   *graph()    const;
+    int      weight()   const;
+    Node    *nodeFrom() const;
+    Node    *nodeTo()   const;
+
+protected:
+    Edge(Graph *graph, int weight, Node *from, Node *to);
+
+    Graph   *_graph;
+    int      _weight;
+    Node    *_from;
+    Node    *_to;
 };
 
 class DirectedEdge : public Edge
 {
 public:
-    DirectedEdge();
+    DirectedEdge(Graph *graph, int weight, Node *from, Node *to);
     virtual ~DirectedEdge();
 };
 
 class UndirectedEdge : public Edge
 {
 public:
-    UndirectedEdge();
+    UndirectedEdge(Graph *graph, int weight, Node *from, Node *to);
     virtual ~UndirectedEdge();
 };
 
