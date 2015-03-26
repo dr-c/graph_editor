311b74544a44c6325d8fda2dae5cb11badfb8948
Vlad <drygbah@gmail.com>
2015-03-26 17:17:04 +0200

    Fill Graph and inherited classes

--- graph.cpp
+++ graph.cpp
@@ -1,4 +1,5 @@
 #include "graph.h"
+
 #include "node.h"
 #include "edge.h"
 
@@ -9,7 +10,20 @@ Graph::Graph()
 
 Graph::~Graph()
 {
+    while (!_nodes.empty())
+        delete _nodes.back();
+    while (!_edges.empty())
+        delete _edges.back();
+}
 
+const std::vector<Node*> &Graph::nodes() const
+{
+    return _nodes;
+}
+
+const std::vector<Edge*> &Graph::edges() const
+{
+    return _edges;
 }
 
 DirectedGraph::DirectedGraph()
@@ -23,6 +37,20 @@ DirectedGraph::~DirectedGraph()
 
 }
 
+Node *DirectedGraph::createNode(int id, int weight, const QPointF &pos)
+{
+    Node *node = new Node(this, id, weight, pos);
+    _nodes.push_back(node);
+    return node;
+}
+
+Edge *DirectedGraph::createEdge(int weight, Node *from, Node *to)
+{
+    Edge *edge = new DirectedEdge(this, weight, from, to);
+    _edges.push_back(edge);
+    return edge;
+}
+
 UndirectedGraph::UndirectedGraph()
     : Graph()
 {
@@ -32,4 +60,18 @@ UndirectedGraph::UndirectedGraph()
 UndirectedGraph::~UndirectedGraph()
 {
 
+}
+
+Node *UndirectedGraph::createNode(int id, int weight, const QPointF &pos)
+{
+    Node *node = new Node(this, id, weight, pos);
+    _nodes.push_back(node);
+    return node;
+}
+
+Edge *UndirectedGraph::createEdge(int weight, Node *from, Node *to)
+{
+    Edge *edge = new UndirectedEdge(this, weight, from, to);
+    _edges.push_back(edge);
+    return edge;
 }
--- graph.h
+++ graph.h
@@ -12,8 +12,20 @@ class Graph
 public:
     virtual ~Graph();
 
+    virtual Node *createNode(int id, int weight, const QPointF &pos) = 0;
+    virtual Edge *createEdge(int weight, Node *from, Node *to) = 0;
+
+    const std::vector<Node*> &nodes() const;
+    const std::vector<Edge*> &edges() const;
+
 protected:
     Graph();
+
+    std::vector<Node*>  _nodes;
+    std::vector<Edge*>  _edges;
+
+private:
+    std::vector<int>    _id_nodes;
 };
 
 class DirectedGraph : public Graph
@@ -21,6 +33,9 @@ class DirectedGraph : public Graph
 public:
     DirectedGraph();
     virtual ~DirectedGraph();
+
+    virtual Node *createNode(int id, int weight, const QPointF &pos);
+    virtual Edge *createEdge(int weight, Node *from, Node *to);
 };
 
 class UndirectedGraph : public Graph
@@ -28,6 +43,9 @@ class UndirectedGraph : public Graph
 public:
     UndirectedGraph();
     virtual ~UndirectedGraph();
+
+    virtual Node *createNode(int id, int weight, const QPointF &pos);
+    virtual Edge *createEdge(int weight, Node *from, Node *to);
 };
 
 #endif // GRAPH_H
