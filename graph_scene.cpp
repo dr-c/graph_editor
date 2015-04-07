6d8921d29d11bedeec00909f4cd035b353f5df30
Vlad <drygbah@gmail.com>
2015-04-07 18:41:08 +0300

    Create Info structs. Create inherited classes ctors and dtors

--- graph_scene.cpp
+++ graph_scene.cpp
@@ -1,12 +1,34 @@
 #include "graph_scene.h"
 
-GraphScene::GraphScene()
+GraphScene::GraphScene(Graph<NodeInfo, EdgeInfo> *graph, QObject *parent)
+    : QGraphicsScene(parent), _graph(graph)
 {
 
 }
 
 GraphScene::~GraphScene()
 {
+    delete _graph;
+}
+
+DirectedGraphScene::DirectedGraphScene(QObject *parent = 0)
+    : GraphScene(parent, new DirectedGraph<NodeInfo, EdgeInfo>())
+{
 
 }
 
+DirectedGraphScene::~DirectedGraphScene()
+{
+
+}
+
+UndirectedGraphScene::UndirectedGraphScene(QObject *parent = 0)
+    : GraphScene(parent, new UndirectedGraph<NodeInfo, EdgeInfo>())
+{
+
+}
+
+UndirectedGraphScene::~UndirectedGraphScene()
+{
+
+}
--- graph_scene.h
+++ graph_scene.h
@@ -3,11 +3,72 @@
 
 #include <QGraphicsScene>
 
+#include "graph.h"
+
+class QGraphicsNode;
+class QGraphicsEdge;
+
+struct NodeInfo;
+struct EdgeInfo;
+
 class GraphScene : public QGraphicsScene
 {
 public:
-    GraphScene();
-    ~GraphScene();
+    GraphScene(Graph<NodeInfo, EdgeInfo> *graph, QObject *parent = 0);
+    virtual ~GraphScene();
+
+private:
+    Graph<NodeInfo, EdgeInfo> *_graph;
+};
+
+class DirectedGraphScene : public GraphScene
+{
+public:
+    DirectedGraphScene(QObject *parent = 0);
+    virtual ~DirectedGraphScene();
+};
+
+class UndirectedGraphScene : public GraphScene
+{
+public:
+    UndirectedGraphScene(QObject *parent = 0);
+    virtual ~UndirectedGraphScene();
+};
+
+
+struct NodeInfo {
+public:
+    NodeInfo() : _weight(1), _graphicsNode(nullptr), _pos(QPointF()) {}
+    virtual ~NodeInfo() {}
+
+    void setWeight(int weight) { _weight = weight; }
+    void setGraphicsNode(QGraphicsNode *gn) {_graphicsNode = gn; }
+    void setPos(const QPointF &pos) { _pos = pos; }
+
+    int weight() const { return _weight; }
+    QGraphicsNode *graphicsNode() const { return _graphicsNode; }
+    QPointF pos() const { return _pos; }
+
+private:
+    int             _weight;
+    QGraphicsNode  *_graphicsNode;
+    QPointF         _pos;
+};
+
+struct EdgeInfo {
+public:
+    EdgeInfo() : _weight(1), _graphicsEdge(nullptr) {}
+    virtual ~EdgeInfo() {}
+
+    void setWeight(int weight) { _weight = weight; }
+    void setGraphicsNode(QGraphicsEdge *ge) { _graphicsEdge = ge; }
+
+    int weight() const { return _weight; }
+    QGraphicsEdge *graphicsEdge() const { return _graphicsEdge; }
+
+private:
+    int             _weight;
+    QGraphicsEdge  *_graphicsEdge;
 };
 
 #endif // GRAPHSCENE_H
