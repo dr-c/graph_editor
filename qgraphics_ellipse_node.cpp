e0fe2e611e11334ce3ef33d990f7a03a4f36fc8d
Vlad <drygbah@gmail.com>
2015-04-08 21:08:15 +0300

    Fix bug with incorrect size of new items.

--- qgraphics_ellipse_node.cpp
+++ qgraphics_ellipse_node.cpp
@@ -6,8 +6,8 @@ QGraphicsEllipseNode::QGraphicsEllipseNode(WeightedNode *node, QGraphicsItem *pa
     _ellipseItem = new QGraphicsEllipseItem(this);
     _idItem = new QGraphicsSimpleTextItem(QString::number(_node->id()), this);
     const QPointF &center = node->pos();
-    _ellipseItem->setRect(center.x() - 10, center.y() - 10, center.x() + 10, center.y() + 10);
-    _idItem->setPos(center.x() - 5, center.y() - 5);
+    _ellipseItem->setRect(center.x() - 10, center.y() - 10, 20, 20);
+    _idItem->setPos(center.x() - 5, center.y() - 10);
 }
 
 QGraphicsEllipseNode::~QGraphicsEllipseNode()
