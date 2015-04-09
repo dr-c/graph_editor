80a5cb871bfb82a8e37fa89c0312787d6f9f65df
Vlad <drygbah@gmail.com>
2015-04-09 11:47:25 +0300

    Add abstract methods, connected to graphic representation.

--- qgraphics_node.cpp
+++ qgraphics_node.cpp
@@ -4,9 +4,35 @@ QGraphicsNode::QGraphicsNode(WeightedNode *node, QGraphicsItem *parent)
     : QGraphicsObject(parent), _node(node)
 {
     _node->setGraphicsNode(this);
+    calcRadius(_node->weight());
 }
 
 QGraphicsNode::~QGraphicsNode()
 {
 
+}
+
+
+qreal QGraphicsNode::radius() const
+{
+    return _radius;
+}
+
+WeightedNode *QGraphicsNode::node() const
+{
+    return _node;
+}
+
+void QGraphicsNode::setWeight(int weight)
+{
+    _node->setWeight(weight);
+    calcRadius(weight);
+    setGeometry(_node->pos());
+}
+
+void QGraphicsNode::calcRadius(int weight)
+{
+    if (weight < 0)
+        weight = 0;
+    _radius = sqrt(weight + RADIUS_SHIFT) * RADIUS_SCALE;
 }
--- qgraphics_node.h
+++ qgraphics_node.h
@@ -9,16 +9,39 @@ class QGraphicsNode : public QGraphicsObject
 {
     Q_OBJECT
 public:
-    virtual ~QGraphicsNode();
+    virtual ~QGraphicsNode() override;
 
     virtual int type() const = 0;
+
     virtual QRectF boundingRect() const = 0;
     virtual void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0) = 0;
+
+    virtual void setPen(const QPen &pen) = 0;
+    virtual void setFont(const QFont &font, const QColor &color = Qt::black) = 0;
+    virtual void setBrush(const QBrush &brush) = 0;
+
+    virtual QPen pen() const = 0;
+    virtual QFont font() const = 0;
+    virtual QBrush brush() const = 0;
+
+    void setWeight(int weight);
+
+    qreal radius() const;
+    WeightedNode *node() const;
 
 protected:
     QGraphicsNode(WeightedNode *node, QGraphicsItem *parent = 0);
 
+    virtual void setGeometry(const QPointF &centerPos) = 0;
+    virtual void calcRadius(int weight);
+
     WeightedNode *_node;
+
+    qreal _radius;
+
+private:
+    static  const       int     RADIUS_SHIFT = 25;
+    static  constexpr   qreal   RADIUS_SCALE = 5.;
 };
 
 #endif // QGRAPHICSNODE_H
