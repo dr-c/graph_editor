a94a96aa330af4a375da84c7e0691a4e6e043b23
Vlad <drygbah@gmail.com>
2015-04-10 20:15:21 +0300

    Add weightItem placement.

--- qgraphics_simple_line_edge.cpp
+++ qgraphics_simple_line_edge.cpp
@@ -1,6 +1,7 @@
 #include "qgraphics_simple_line_edge.h"
 
 #include "qgraphics_node.h"
+#include "weight_text_item.h"
 
 QGraphicsSimpleLineEdge::QGraphicsSimpleLineEdge(WeightedEdge *edge, QGraphicsItem *parent)
     : QGraphicsEdge(edge, parent)
@@ -20,9 +21,13 @@ int QGraphicsSimpleLineEdge::type() const
 
 void QGraphicsSimpleLineEdge::draw(QGraphicsNode *fromNode, QGraphicsNode *toNode)
 {
-    QPainterPath path(fromNode->calcIntermediatePoint(toNode->node()->pos()));
-    path.lineTo(toNode->calcIntermediatePoint(fromNode->node()->pos()));
+    const QPointF from_interm_point = fromNode->calcIntermediatePoint(toNode->node()->pos());
+    const QPointF to_interm_point = toNode->calcIntermediatePoint(fromNode->node()->pos());
+    QPainterPath path(from_interm_point);
+    path.lineTo(to_interm_point);
     setPath(path);
+    _weightItem->calcCenterPoint(QRectF(from_interm_point, to_interm_point));
+    _weightItem->placeInCenter();
 }
 
 void QGraphicsSimpleLineEdge::draw(QGraphicsNode *fromNode, const QPointF &toPoint)
